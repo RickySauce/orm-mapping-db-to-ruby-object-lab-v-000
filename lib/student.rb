22a035516349761387396d11e2dff712dda1572c
Eric DeSimone <eric.desimone@cortland.edu>
2018-06-25 10:53:10 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -35,6 +35,16 @@ class Student
     end.first
   end
 
+  def self.count_all_students_in_grade_9
+    sql = <<-SQL
+      SELECT COUNT(grade)
+      FROM students
+      WHERE grade = 9
+    SQL
+
+      DB[:conn].execute(sql)
+  end
+
   def save
     sql = <<-SQL
       INSERT INTO students (name, grade)
